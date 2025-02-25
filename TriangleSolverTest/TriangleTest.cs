e6d5b0b0d9a9fc4f7bbc833c35b7904948edfdd4
dhwaniranderi <dhwaniranderi96@gmail.com>
2025-02-25 14:58:09 -0500

    Add project files.

---
+++ TriangleSolverTest/TriangleTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TriangleSolver;
+using NUnit;
+using NUnit.Framework;
+//using NUnitDemonstration;
+
+namespace TriangleSolverTest
+{
+    [TestFixture]
+    public class TriangleTest
+    {
+        [Test]
+        public void Test1()
+        {
+            // Arrange
+            int firstAngle = 3;
+            int secondAngle = 3;
+            int thirdAngle = 3;
+            string expected = "Equilateral triangle";
+
+            // Act
+            string actual = TriangleSolver.Triangle.AnalyzeTriangle(firstAngle, secondAngle, thirdAngle);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
